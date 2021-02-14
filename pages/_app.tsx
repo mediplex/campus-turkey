c3abf707fd0974293d99476b14357e996ba90388
Mehdi Karim <contact@mehdikarim.com>
2021-02-14 21:13:57 +0000

    chore(global): remove tailwind and integrate material-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,42 @@
-import "tailwindcss/tailwind.css";
-
+import React from 'react';
 import { IntlProvider } from 'react-intl';
 import { polyfill } from '../polyfills';
 
 import App from 'next/app';
+import Head from 'next/head';
+
+import { ThemeProvider } from '@material-ui/core/styles';
+import { CacheProvider } from '@emotion/react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import createCache from '@emotion/cache';
+import theme from '../src/theme';
+
+export const cache = createCache({ key: 'css', prepend: true });
 
 function MyApp({ Component, pageProps, locale, messages }) {
-    return (
-        <IntlProvider locale={locale} defaultLocale="en" messages={messages}>
-            <Component {...pageProps} />
-        </IntlProvider>
-    );
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <IntlProvider locale={locale} defaultLocale="en" messages={messages}>
+      <CacheProvider value={cache}>
+        <Head>
+          <title>My page</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </IntlProvider>
+  );
 }
 
 /**
@@ -22,62 +48,62 @@ function MyApp({ Component, pageProps, locale, messages }) {
  * and the promise of fetching the translated messages
  */
 function getMessages(locales: string | string[] = ['en']) {
-    if (!Array.isArray(locales)) {
-        locales = [locales];
+  if (!Array.isArray(locales)) {
+    locales = [locales];
+  }
+  let langBundle;
+  let locale;
+  for (let i = 0; i < locales.length && !locale; i++) {
+    locale = locales[i];
+    switch (locale) {
+      case 'fr':
+        langBundle = import('../compiled-lang/fr.json');
+        break;
+      case 'en-GB':
+        langBundle = import('../compiled-lang/en-GB.json');
+        break;
+      case 'zh-Hans-CN':
+        langBundle = import('../compiled-lang/zh-Hans-CN.json');
+        break;
+      case 'zh-Hant-HK':
+        langBundle = import('../compiled-lang/zh-Hant-HK.json');
+        break;
+      default:
+        break;
+      // Add more languages
     }
-    let langBundle;
-    let locale;
-    for (let i = 0; i < locales.length && !locale; i++) {
-        locale = locales[i];
-        switch (locale) {
-            case 'fr':
-                langBundle = import('../compiled-lang/fr.json');
-                break;
-            case 'en-GB':
-                langBundle = import('../compiled-lang/en-GB.json');
-                break;
-            case 'zh-Hans-CN':
-                langBundle = import('../compiled-lang/zh-Hans-CN.json');
-                break;
-            case 'zh-Hant-HK':
-                langBundle = import('../compiled-lang/zh-Hant-HK.json');
-                break;
-            default:
-                break;
-            // Add more languages
-        }
-    }
-    if (!langBundle) {
-        return ['en', import('../compiled-lang/en.json')];
-    }
-    return [locale, langBundle];
+  }
+  if (!langBundle) {
+    return ['en', import('../compiled-lang/en.json')];
+  }
+  return [locale, langBundle];
 }
 
 const getInitialProps: typeof App.getInitialProps = async (appContext) => {
-    const {
-        ctx: { req },
-    } = appContext;
-    const requestedLocales: string | string[] =
-        (req as any)?.locale ||
-        (typeof navigator !== 'undefined' && navigator.languages) ||
-        // IE11
-        (typeof navigator !== 'undefined' && (navigator as any).userLanguage) ||
-        (typeof window !== 'undefined' && (window as any).LOCALE) ||
-        'en';
+  const {
+    ctx: { req },
+  } = appContext;
+  const requestedLocales: string | string[] =
+    (req as any)?.locale ||
+    (typeof navigator !== 'undefined' && navigator.languages) ||
+    // IE11
+    (typeof navigator !== 'undefined' && (navigator as any).userLanguage) ||
+    (typeof window !== 'undefined' && (window as any).LOCALE) ||
+    'en';
 
-    const [supportedLocale, messagePromise] = getMessages(requestedLocales);
+  const [supportedLocale, messagePromise] = getMessages(requestedLocales);
 
-    const [, messages, appProps] = await Promise.all([
-        polyfill(supportedLocale),
-        messagePromise,
-        App.getInitialProps(appContext),
-    ]);
+  const [, messages, appProps] = await Promise.all([
+    polyfill(supportedLocale),
+    messagePromise,
+    App.getInitialProps(appContext),
+  ]);
 
-    return {
-        ...(appProps as any),
-        locale: supportedLocale,
-        messages: messages.default,
-    };
+  return {
+    ...(appProps as any),
+    locale: supportedLocale,
+    messages: messages.default,
+  };
 };
 
 MyApp.getInitialProps = getInitialProps;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,56 @@
 import * as React from 'react';
 import { FormattedMessage, FormattedNumber, useIntl } from 'react-intl';
 import Head from 'next/head';
-import Layout from '../components/Layout';
+import Layout from '../src/components/Layout';
+
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+import { Button } from '@material-ui/core';
 
 export default function Home() {
-    const intl = useIntl();
-
-    return (
-        <Layout
-            title={intl.formatMessage({
-                defaultMessage: 'Home',
-            })}
-        >
-            <Head>
-                <meta
-                    name="description"
-                    content={intl.formatMessage({
-                        defaultMessage:
-                            'An example app integrating React Intl with Next.js',
-                    })}
-                />
-            </Head>
-            <p>
-                <FormattedMessage defaultMessage="Hello, World!" />
-            </p>
-            <p>
-                <FormattedNumber value={1000} />
-            </p>
-        </Layout>
-    );
+  return (
+    <>
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Next.js v5-alpha with TypeScript example
+          </Typography>
+        </Box>
+      </Container>
+      <Container>
+        <WithLayout />
+        <Button variant="contained">Primary</Button>
+      </Container>
+    </>
+  );
 }
+
+const WithLayout = () => {
+  const intl = useIntl();
+
+  return (
+    <Layout
+      title={intl.formatMessage({
+        defaultMessage: 'Home',
+      })}
+    >
+      <Head>
+        <meta
+          name="description"
+          content={intl.formatMessage({
+            defaultMessage:
+              'An example app integrating React Intl with Next.js',
+          })}
+        />
+      </Head>
+      <p>
+        <FormattedMessage defaultMessage="Hello, World!" />
+      </p>
+      <p>
+        <FormattedNumber value={1000} />
+      </p>
+    </Layout>
+  );
+};
